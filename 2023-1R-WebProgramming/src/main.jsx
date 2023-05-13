c7b7a189ef444cf23093f93c05764f0670e3fdbb
lys3269 <lys3269@dgu.ac.kr>
2023-05-14 01:12:06 +0900

    Add 일반함수 vs 화살표함수 과제

--- 2023-1R-WebProgramming/src/main.jsx
+++ 2023-1R-WebProgramming/src/main.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-//import Worldcup from "./Worldcup.jsx";
+//import App from "./App.jsx";
+import Worldcup from "./Worldcup.jsx";
 
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Worldcup />
   </React.StrictMode>
 );
